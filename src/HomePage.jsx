e8bcc89b85d059e8c7b70a58a43cd3fe4c6b8cbf
RobynSpaulding <robynm27@gmail.com>
2023-03-29 12:11:33 -0700

    fixes homePage price

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -8,7 +8,7 @@ export function HomePage() {
       <img src={homeImage} style={{ width: "60%" }} alt="image" />
       <p className="row justify-content-center">
         Pelicans in Cypress Trees &nbsp;&nbsp;|&nbsp;&nbsp; Pastel &nbsp;&nbsp;|&nbsp;&nbsp; 11 x
-        14&nbsp;&nbsp;|&nbsp;&nbsp; $650.00
+        14&nbsp;&nbsp;|&nbsp;&nbsp; $750.00
       </p>
       <div style={{ display: "flex", alignItems: "center", marginTop: "-200px" }}>
         <div style={{ flex: 1, backgroundColor: "#787255", height: "3px" }} />
